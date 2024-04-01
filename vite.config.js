1d8491833b9532ed7b533f91887b945f1624cc30
Asad Sharif <asadsharif1270@gmail.com>
2024-04-02 00:42:33 +0500

    business card

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/business-card/', // Replace 'repository-name' with your GitHub repository name
+
   plugins: [react()],
 })
